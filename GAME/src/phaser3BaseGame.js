2b4e3221d4e232952f3a00b5ed3b5abf29e8825e
AdrianAlbarran <adrian-240102@hotmail.es>
2022-11-03 13:36:11 +0100

    Carpetas organizadas y añadido intelliCode

--- GAME/src/phaser3BaseGame.js
+++ GAME/src/phaser3BaseGame.js
@@ -1,3 +1,10 @@
+/*
+* Esto de abajo es para añadir intelliCode de phaser, debe estar en cada js, con la ruta 
+* relativa correcta 
+*/ 
+
+/** @type { import ("../typings/phaser") } */
+
 
 var config = {
     type: Phaser.AUTO,
