2bfff8d29150960d43874bda62741a1cb69fb098
Adrian <adrianserranomonteiro2@gmail.com>
2022-11-28 19:17:54 +0100

    icons gameover

--- GAME/src/scenes/GameOver.js
+++ GAME/src/scenes/GameOver.js
@@ -6,26 +6,52 @@ class GameOver extends Phaser.Scene {
     }
 
     create() {
-       this.add.image(400, 300, "gameOver");
+        const bcSelect = this.sound.add("MENUSELECT", 
+        { 
+            loop: false,
+            mute: false,
+            volume: 0.20
+        });
 
-       let mainMenu = this.add.text(400, 200, "EXIT", {
+       this.add.image(400, 300, "gameOver");
+       let mouseSpriteMain = this.add.image(495, 200, "sugarIcon");
+       let mouseSpriteRestart = this.add.image(555, 250, "sugarIcon");
+
+       mouseSpriteMain.setVisible(false);
+       mouseSpriteRestart.setVisible(false);
+
+       let mainMenu = this.add.text(420, 200, "EXIT", {
             fontSize: '26px', fill: '#j78999', fontFamily: 'Pixel'
         }).setOrigin(0.5).setInteractive();
 
-        let restart = this.add.text(400, 250, "RESTART", {
+        let restart = this.add.text(440, 250, "RESTART", {
             fontSize: '26px', fill: '#j78999', fontFamily: 'Pixel'
         }).setOrigin(0.5).setInteractive();
 
         mainMenu.on('pointerdown', () => {
+            bcSelect.play();
             wave = 1;
             this.scene.start("mainMenu");
             endGameMusic.pause();
         });
+        mainMenu.on('pointerover', () => {
+            mouseSpriteMain.setVisible(true);
+        });
+        mainMenu.on('pointerout', () => {
+            mouseSpriteMain.setVisible(false);
+        });
 
         restart.on('pointerdown', () => {
+            bcSelect.play();
             wave = 1;
             this.scene.start("mainScene");
             endGameMusic.pause();
+        });
+        restart.on('pointerover', () => {
+            mouseSpriteRestart.setVisible(true);
+        });
+        restart.on('pointerout', () => {
+            mouseSpriteRestart.setVisible(false);
         });
     }
 }
