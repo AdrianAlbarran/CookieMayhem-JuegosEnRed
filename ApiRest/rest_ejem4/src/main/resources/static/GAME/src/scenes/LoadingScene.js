fb85bd3d5f39f42e4e6ba09b87035c60b3dba4d7
AdrianAlbarran <adrian-240102@hotmail.es>
2022-12-19 23:14:03 +0100

    Todo funciona bien

--- ApiRest/rest_ejem4/src/main/resources/static/GAME/src/scenes/LoadingScene.js
+++ ApiRest/rest_ejem4/src/main/resources/static/GAME/src/scenes/LoadingScene.js
@@ -12,80 +12,80 @@ class LoadingScene extends Phaser.Scene
         // TODO: PRELOAD ALL THE NECCESARY BASE ASSETS HERE
         //Waiting to  
         
-        this.load.image("BACKGROUND", "assets/fondo.png");
+        this.load.image("BACKGROUND", "GAME/assets/fondo.png");
         // * BALAS
-        this.load.image('cafe', 'assets/balas/cafe.png');
+        this.load.image('cafe', 'GAME/assets/balas/cafe.png');
         
         //...
-        this.load.image("CHIPCOOKIE",'assets/enemigos/cookie/cookie.png');
-        this.load.image("OREOCOOKIE",'assets/enemigos/oreo/oreo.png');
-        this.load.image("FRUITCOOKIE",'./assets/enemigos/fruta/fruta.png');
-        this.load.image("DINOCOOKIE",'assets/enemigos/dino/dino.png');
-        this.load.image("GINGERCOOKIE",'assets/enemigos/ginger/ginger.png');
+        this.load.image("CHIPCOOKIE",'GAME/assets/enemigos/cookie/cookie.png');
+        this.load.image("OREOCOOKIE",'GAME/assets/enemigos/oreo/oreo.png');
+        this.load.image("FRUITCOOKIE",'GAME/assets/enemigos/fruta/fruta.png');
+        this.load.image("DINOCOOKIE",'GAME/assets/enemigos/dino/dino.png');
+        this.load.image("GINGERCOOKIE",'GAME/assets/enemigos/ginger/ginger.png');
 
-        this.load.image("PMENU","assets/menu.png");
+        this.load.image("PMENU","GAME/assets/menu.png");
 
         //GAMEOVER
-        this.load.image("gameOver", "assets/gameover.png")
+        this.load.image("gameOver", "GAME/assets/gameover.png")
 
         // * JUGADOR
-        this.load.spritesheet("PLAYER1", "./assets/jugadores/Jugador_1/spriteP1Corriendo.png", {frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet("PLAYER2", "./assets/jugadores/Jugador_2/spriteSCorriendoP2.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("PLAYER1", "GAME/assets/jugadores/Jugador_1/spriteP1Corriendo.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("PLAYER2", "GAME/assets/jugadores/Jugador_2/spriteSCorriendoP2.png", {frameWidth: 64, frameHeight: 64 });
 
         //CORAZON PLAYER
-        this.load.image("HP", "assets/interface/CorazonNuevoP1.png");
-        this.load.image("HPH", "assets/interface/CorazonPartidoNuevoP1.png");
-        this.load.image("HP1", "assets/interface/CorazonNuevoP2.png");
-        this.load.image("HPH1", "assets/interface/CorazonPartidoNuevoP2.png");
+        this.load.image("HP", "GAME/assets/interface/CorazonNuevoP1.png");
+        this.load.image("HPH", "GAME/assets/interface/CorazonPartidoNuevoP1.png");
+        this.load.image("HP1", "GAME/assets/interface/CorazonNuevoP2.png");
+        this.load.image("HPH1", "GAME/assets/interface/CorazonPartidoNuevoP2.png");
 
         // * ENEMIGOS
-        this.load.spritesheet("CCANIM", "./assets/enemigos/cookie/cookie_spritesheet.png", {frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet("OCANIM", "./assets/enemigos/oreo/oreo_sprittesheet.png", {frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet("FCANIM", "./assets/enemigos/fruta/fruta_sprittesheet.png", {frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet("DCANIM", "./assets/enemigos/dino/dinoSprite.png", {frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet("GCANIM", "./assets/enemigos/ginger/GingerSprite.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("CCANIM", "GAME/assets/enemigos/cookie/cookie_spritesheet.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("OCANIM", "GAME/assets/enemigos/oreo/oreo_sprittesheet.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("FCANIM", "GAME/assets/enemigos/fruta/fruta_sprittesheet.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("DCANIM", "GAME/assets/enemigos/dino/dinoSprite.png", {frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet("GCANIM", "GAME/assets/enemigos/ginger/GingerSprite.png", {frameWidth: 64, frameHeight: 64 });
         
         // * TIENDA
-        this.load.image("TIENDA", "./assets/tetera.png");
-        this.load.image("BMENU","./assets/iconos/botonLuisChiquito.png");
-        this.load.image("BMENU1","./assets/iconos/botonLuisChiquito1.png");
-        this.load.image("BMENU2","./assets/iconos/botonLuisChiquito2.png");
-        this.load.image("IDMG","./assets/iconos/iconoMasDaño.png");
-        this.load.image("IMB","./assets/iconos/iconoMasBalas.png");
-        this.load.image("IVA","./assets/iconos/iconoMasVelDeAtq.png");
-        this.load.image("IVM","./assets/iconos/iconoMasVelDeMovimiento.png");
+        this.load.image("TIENDA", "GAME/assets/tetera.png");
+        this.load.image("BMENU","GAME/assets/iconos/botonLuisChiquito.png");
+        this.load.image("BMENU1","GAME/assets/iconos/botonLuisChiquito1.png");
+        this.load.image("BMENU2","GAME/assets/iconos/botonLuisChiquito2.png");
+        this.load.image("IDMG","GAME/assets/iconos/iconoMasDaño.png");
+        this.load.image("IMB","GAME/assets/iconos/iconoMasBalas.png");
+        this.load.image("IVA","GAME/assets/iconos/iconoMasVelDeAtq.png");
+        this.load.image("IVM","GAME/assets/iconos/iconoMasVelDeMovimiento.png");
         // * SONIDOS
         // ? Disparar
-        this.load.audio("SHOOT", "./assets/sonidos/Pasos.mp3");
+        this.load.audio("SHOOT", "GAME/assets/sonidos/Pasos.mp3");
         // * Daño a enemigos
-        this.load.audio("COOKIEDAMAGED", "./assets/sonidos/Daño_Galletas.mp3");
+        this.load.audio("COOKIEDAMAGED", "GAME/assets/sonidos/Daño_Galletas.mp3");
         // * Daño a Jugador
-        this.load.audio("PLAYERDAMAGE", "./assets/sonidos/Daño_Tazas.mp3");
+        this.load.audio("PLAYERDAMAGE", "GAME/assets/sonidos/Daño_Tazas.mp3");
         // * Pasos Jugador
-        this.load.audio("PLAYERSTEPS", "assets/sonidos/Pasos.mp3");
+        this.load.audio("PLAYERSTEPS", "GAME/assets/sonidos/Pasos.mp3");
         // * Dinero
-        this.load.audio("MONEYSOUND", "./assets/sonidos/CogerDinero.mp3");
+        this.load.audio("MONEYSOUND", "GAME/assets/sonidos/CogerDinero.mp3");
         // * Comprar Tienda
-        this.load.audio("BUYSHOP", "./assets/sonidos/Coger_Tienda.mp3");
-        this.load.audio("FAILSHOP", "./assets/sonidos/Fallar_Tienda.mp3");
+        this.load.audio("BUYSHOP", "GAME/assets/sonidos/Coger_Tienda.mp3");
+        this.load.audio("FAILSHOP", "GAME/assets/sonidos/Fallar_Tienda.mp3");
 
         // * Background music
-        this.load.audio("GAMEMUSIC", "./assets/musica/Cancion_JeR_1.mp3");
-        this.load.audio("GAMEOVERMUSIC", "assets/musica/Cancion_Derrota.mp3");
+        this.load.audio("GAMEMUSIC", "GAME/assets/musica/Cancion_JeR_1.mp3");
+        this.load.audio("GAMEOVERMUSIC", "GAME/assets/musica/Cancion_Derrota.mp3");
 
         // * UI Health Bar
-        this.load.image('left-cap', 'assets/interface/Barra Izquierda.png');
-        this.load.image('middle', 'assets/interface/Barra Medio.png');
-        this.load.image('right-cap', 'assets/interface/Barra Derecha.png');
+        this.load.image('left-cap', 'GAME/assets/interface/Barra Izquierda.png');
+        this.load.image('middle', 'GAME/assets/interface/Barra Medio.png');
+        this.load.image('right-cap', 'GAME/assets/interface/Barra Derecha.png');
     
-        this.load.image('left-cap-shadow', 'assets/interface/barHorizontal_shadow_left.png');
-        this.load.image('middle-shadow', 'assets/interface/barHorizontal_shadow_mid.png');
-        this.load.image('right-cap-shadow', 'assets/interface/barHorizontal_shadow_right.png');;
+        this.load.image('left-cap-shadow', 'GAME/assets/interface/barHorizontal_shadow_left.png');
+        this.load.image('middle-shadow', 'GAME/assets/interface/barHorizontal_shadow_mid.png');
+        this.load.image('right-cap-shadow', 'GAME/assets/interface/barHorizontal_shadow_right.png');;
 
         // * UI Armas
-        this.load.image('ICONOESCOPETA', './assets/iconos/iconoEscopeta.png');
-        this.load.image('ICONOREVOLVER', './assets/iconos/iconoRevolver.png');
-        this.load.image('ICONOSUBFUSIL', './assets/iconos/iconoSubfusil.png');
+        this.load.image('ICONOESCOPETA', 'GAME/assets/iconos/iconoEscopeta.png');
+        this.load.image('ICONOREVOLVER', 'GAME/assets/iconos/iconoRevolver.png');
+        this.load.image('ICONOSUBFUSIL', 'GAME/assets/iconos/iconoSubfusil.png');
         
 
         
---
+++ README.md
@@ -0,0 +1,166 @@
+# COOKIE MAYHEM - JUEGOS EN RED
+
+Este proyecto está sujeto a cambios.
+
+Somos un grupo de estudiantes de Diseño y Desesarrollo de Videojuegos en la Universidad Rey Juan Carlos desarrollando un juego multijugador con Phaser3.0
+
+- Concepto principal:
+ Bullet Hell cooperativo 2D con estética pastelera donde el objetivo es defender tú base de oleadas de galletas que atacan la base y a los jugadores. Según los jugadores consiguen recursos, mejoran sus armas y habilidades. Estas van convirtiendo el juego en un bullet hell según las mejoras.
+
+- Estilo Visual:
+Se utilizará una estética "muy azucarada", donde se utilizarán colores planos, formas redonda estética relacionada con la repostería.
+
+- Gameplay:
+El juego sostiene un sistema de oleados en la que diversos enemigos intentaran destruir la estructura central donde los jugadores deberán defenderla disparando frenéticamente, después de cada ronda los jugadores podrán acceder a una tienda para comprar mejoras para las rondas posteriores.
+
+- Sonido:
+Tanto el estilo de la música como el estilo de los sonidos será en 8 bits.
+Exisistirán diversos tonos durante todo el juego dependiedo del estado de este:
+ -El menú tendrá una música más relajada
+ -Durante el gameplay, la música será radicalmente distinta.
+ -La música de fin del juego será una melodía triste
+
+- Historia:
+En el reino chuchería las galletas se rebelan contra el reino para detener la masacre de galletas que se lleva a cabo en la gran tetera desde tiempos inmemorables.
+Sois los últimos soldados de la legión expresso y vuestro deber la revolución o morir en el intento.
+
+
+- Motor de Juego:
+Usaremos <a href = "https://phaser.io/phaser3"> Phaser3 </a> 
+- Coordinación del equipo.
+Para la coordinación del equipo se usará Trello, usando <a href = "https://trello.com/invite/b/JZrPiCv6/ATTI46729dd3a9ab2625894f6d3f58aaac69019100A5/juegos-en-red">este tablero</a> .
+
+
+ ## Team:
+ <table>
+  <thead>
+    <tr>
+      <th> INTEGRANTES </th> 
+      <th> CORREO UNIVERSITARIO </th> 
+      <th> GITHUB </th> 
+      <th> ROL </th> 
+    </tr>
+  </thead>
+  <tbody>
+    <tr> 
+      <td> Adrián Albarrán Alcalde </td>
+      <td> a.albarran.2020@alumnos.urjc.es </td>
+      <td> <a href = "https://github.com/AdrianAlbarran"> AdrianAlbarran </a> </td>
+      <td> Lider Proyecto </td>
+    </tr>
+    <tr> 
+      <td> Luis Morón Álvarez </td>
+      <td> l.moron.2020@alumnos.urjc.es </td>
+      <td> <a href = "https://github.com/Moromon"> Moromon </a> </td>
+      <td> Programador </td>
+    </tr>
+    <tr> 
+      <td> Gloria Turati Domínguez </td>
+      <td> g.turati.2020@alumnos.urjc.es </td>
+      <td> <a href = "https://github.com/glorimm"> Glorimm </a> </td>
+      <td> Arte </td>
+    </tr>
+    <tr> 
+      <td> Marco Ozaeta Velasco </td>
+      <td> m.ozaeta.2020@alumnos.urjc.es </td>
+      <td> <a href = "https://github.com/Marcooza"> Marcooza </a> </td>
+      <td> Guionista </td>
+    </tr>
+    <tr> 
+      <td> Adrián Serrano Monteiro </td>
+      <td> a.serranom.2020@alumnos.urjc.es </td>
+      <td> <a href = "https://github.com/PinguinoTocho"> PinguinoTocho </a> </td>
+      <td> Redes sociales y Arte </td>
+    </tr>
+    <tr> 
+      <td> Daniel Salguero Fernández </td>
+      <td> d.salguero.2020@alumnos.urjc.es </td>
+      <td> <a href = "https://github.com/DaniSF1"> DaniSF1 </a> </td>
+      <td> Música </td>
+    </tr>
+   </tbody>
+  </table>
+
+
+ ## Controles del Juego:
+ 
+![Controles Cookie Mayhem](https://user-images.githubusercontent.com/115086690/204564478-79935e8f-d9c1-4fed-bfc3-10086d00edc6.png)
+
+## Diagrama de navegación
+
+Este diagrama indica los estados del juego y cómo se relacionan entre ellos, iniciando y acabando en la pantalla de inicio
+
+![image10](https://user-images.githubusercontent.com/93784360/204644030-d96a0419-2dc5-407f-b56e-acacba258eba.png)
+
+## Pantalla de Inicio
+
+![image16](https://user-images.githubusercontent.com/93784360/204644119-886b5d1f-b6f3-4c99-8ec7-f33f8c74bde4.png)
+
+El juego comienza con una pantalla de menú de inicio que ofrece las siguientes posibilidades: el botón PLAY, que avanza hacia la escena de gameplay, el botón SETTINGS, que por el momento permite silenciar el sonido del juego, un botón para activar la pantalla completa y otro botón en la parte inferior derecha para ver los controles del juego.
+
+## Pantalla de Ajustes
+
+![image13](https://user-images.githubusercontent.com/93784360/204644134-dd6104b7-2780-46f1-aa91-d209a1cbbb1e.png)
+
+
+Al presionar el botón de SETTINGS se muestra un menú que permite silenciar el juego y un botón de EXIT para regresar al menú principal.
+
+## Pantalla de Controles
+
+![image12](https://user-images.githubusercontent.com/93784360/204644140-71d7db35-721c-474d-ac17-e48ba9a1caa0.png)
+
+
+Al presionar el botón de controles, se muestran en pantalla los controles del juego. Al volver a presionar este botón, se vuelve al menú principal.
+
+
+## Gameplay
+
+
+Al presionar el botón PLAY comienza una escena en la que el jugador puede interactuar con el escenario, progresar en el sistema de oleadas y abrir la tienda para comprar mejoras.
+
+![image7](https://user-images.githubusercontent.com/93784360/204644190-945b855e-9c38-4282-80cc-cf0b27e841df.png)
+
+
+La tienda del juego se puede abrir presionando la tecla P, desde esta pantalla se pueden comprar objetos a cambio de la moneda del juego para obtener mejoras de velocidad de movimiento, daño, más proyectiles por disparo y cadencia de disparo.
+
+
+![image1](https://user-images.githubusercontent.com/93784360/204644208-328645f7-9838-486f-87f6-4fb164ac1d4b.png)
+
+
+## Pantalla de Derrota
+
+![image15](https://user-images.githubusercontent.com/93784360/204644221-c6029506-2ccc-448c-8040-0da203a2b49e.png)
+
+
+La escena cambia a la pantalla de derrota cuando la vida de la tetera llega a 0 o cuándo ambos jugadores son eliminados. Desde esta pantalla se puede regresar al menú de inicio mediante el botón EXIT o volver a reiniciar el gameplay desde el botón RESTART.
+
+## Diseño de Enemigos, Personajes y Mejoras
+
+## Enemigos
+
+![image9](https://user-images.githubusercontent.com/93784360/204644243-75009f2b-4770-46c1-8b61-004ca3b51416.png)
+![image8](https://user-images.githubusercontent.com/93784360/204644251-57ee0afc-e85a-432c-a0fd-6f5ce8516eb8.png)
+![image17](https://user-images.githubusercontent.com/93784360/204644267-d967f1b7-94bf-451a-9632-81437fca5c32.png)
+![image2](https://user-images.githubusercontent.com/93784360/204644292-61ea7eeb-445e-4a4e-becc-b0cf35d49a66.png)
+![image5](https://user-images.githubusercontent.com/93784360/204644304-6386e182-05f1-4bf3-94de-5e2b5f2c4abb.png)
+
+
+## Player y tetera
+
+![image4](https://user-images.githubusercontent.com/93784360/204644318-e9d8732f-ccb7-4957-a98c-270a3468e063.png)
+![image21](https://user-images.githubusercontent.com/93784360/204644351-ae23cb98-f139-453f-91f8-92f1d886b37e.png)
+![image19](https://user-images.githubusercontent.com/93784360/204644358-ff386dfe-d1e4-490f-8c1d-78a8a1e2a9ce.png)
+
+
+## Iconos
+
+![image20](https://user-images.githubusercontent.com/93784360/204644370-6a91e800-82a2-4934-a886-980cdac6d890.png)
+![image18](https://user-images.githubusercontent.com/93784360/204644373-7cc74687-ade4-4454-8a4a-db6ccab6d1a6.png)
+![iconoEscopeta](https://user-images.githubusercontent.com/93784360/204644507-038bd81e-bcb8-4f35-886f-0022a55557da.png)
+![image14](https://user-images.githubusercontent.com/93784360/204644554-56a4f5d3-554b-4775-8f26-e6c55c5c37b4.png)
+![image3](https://user-images.githubusercontent.com/93784360/204644561-33c1bf16-0ac5-4532-a99b-62b2ae7a190d.png)
+![image11](https://user-images.githubusercontent.com/93784360/204644570-e1f4936f-daed-4e72-8125-590c8c9bc008.png)
+![image6](https://user-images.githubusercontent.com/93784360/204644584-671d8d2e-1471-414a-bc7f-c99887e5823b.png)
+
+
+
