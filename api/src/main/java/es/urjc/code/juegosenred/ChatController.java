c24033916fde1906bc56936738799a7bccb04d90
Moromon <lmoralvxd@gmail.com>
2022-12-20 21:59:21 +0100

    Actualizacion Directorio y .jar

---
+++ api/src/main/java/es/urjc/code/juegosenred/ChatController.java
@@ -0,0 +1,56 @@
+package es.urjc.code.juegosenred;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/messages")
+public class ChatController {
+	
+	CreateJson json = new CreateJson();
+	
+	Map<Long, Message> messages = new ConcurrentHashMap<>(); 
+	AtomicLong nextId = new AtomicLong(0);
+	
+	@GetMapping
+	public Collection<Message> AJAXGETmessages() {
+		return messages.values();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Message AJAXPOST_newMessage(@RequestBody Message message) {
+
+		long id = nextId.incrementAndGet();
+		message.setId(id);
+		if(message.getType() != 101 && message.getType() != 200 && message.getType() != 201)
+		{
+			message.setType(101);
+		}
+		messages.put(id, message);
+		json.main(message);
+		
+
+		return message;
+	}
+
+	
+}
