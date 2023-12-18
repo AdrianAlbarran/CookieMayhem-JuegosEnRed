bba2c43bf5fd8318fef3dff1479dde50fafd523a
Moromon <lmoralvxd@gmail.com>
2023-12-18 19:54:26 +0100

    Cosas que de chat

--- api/src/main/resources/static/GAME/src/webSockets/WebSocketsGroup.js
+++ api/src/main/resources/static/GAME/src/webSockets/WebSocketsGroup.js
@@ -1,29 +1,29 @@
 // SOCKET PARA MOVEMENT
 var aux = window.location.href;
-console.log(aux);
-var movIP = 'ws://127.0.0.1:8080/movement';
+console.log('ws:'+window.location.href+'movement');
+var movIP = 'ws:'+window.location.href+'movement';
 var wsMov = new WebSocket(movIP);
 
 // SOCKET PARA DISPAROS
-var shootIP = 'ws://127.0.0.1:8080/shoot';
+var shootIP = 'ws:'+window.location.href+'shoot';
 var wsShoot = new WebSocket(shootIP);
 
 // SOCKET PARA GENERAR ENEMIGOS
-var genEnemIP = 'ws://127.0.0.1:8080/genenemy';
+var genEnemIP = 'ws://'+window.location.href+'genenemy';
 var wsGenEnem = new WebSocket(genEnemIP);
 
 // SOCKET PARA COMPRARS EN LA TIENDA Y UPDATE DEL SCORE
-var shopIP = 'ws://127.0.0.1:8080/shop';
+var shopIP = 'ws://'+window.location.href+'shop';
 var wsShop = new WebSocket(shopIP); 
 
 // SOCKET PARA LAS ARMAS
-var weaponIP = 'ws://127.0.0.1:8080/weapon';
+var weaponIP = 'ws://'+window.location.href+'weapon';
 var wsWeapon = new WebSocket(weaponIP); 
 
 // SOCKET PARA EL LOBBY
-var lobbyIP = 'ws://127.0.0.1:8080/lobby';
+var lobbyIP = 'ws://'+window.location.href+'lobby';
 var wsLobby; // Inicializada al darle al boton "play"
 
 // SOCKET PARA EL DSYNC
-var dsyncIP = 'ws://127.0.0.1:8080/dsync';
+var dsyncIP ='ws://'+window.location.href+'dsync';
 var wsDsync = new WebSocket(dsyncIP);  // Inicializada al darle al boton "play"
